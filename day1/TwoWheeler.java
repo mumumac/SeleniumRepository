367c6b73fa2152c5ed66694d21ab61d9fee2ddb4
Moorthy13 <118475473+mumumac@users.noreply.github.com>
2023-02-04 22:05:27 +0530

    Add files via upload

---
+++ day1/TwoWheeler.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class TwoWheeler {
+	public static void main(String[]args) {
+		System.out.println("MOORTHY");
+		int i = 93;
+		boolean inputSix = false;
+		float no = 55;
+		String name = "JOHAN MARC";
+		long number = 2342566433324l;
+		short noofmobiles = 26;
+		System.out.println(inputSix);
+		System.out.println(no);
+		System.out.println(name);
+		System.out.println(number);
+		System.out.println(noofmobiles);
+	}
+	
+}
+	
+		
