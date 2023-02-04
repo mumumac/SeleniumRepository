5bb9e25b2c3f7cf36e6adfdbbfb41860c262df96
Moorthy13 <118475473+mumumac@users.noreply.github.com>
2023-02-04 22:13:24 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,22 @@
+
+public class Mobile {
+public static void main (String[]args) {
+	String mobileBrandName = "IPHONE 11";
+	System.out.println(mobileBrandName);
+	char mobilelogo = 'I';
+	System.out.println(mobilelogo);
+	short noofMobilePiece = 133;
+	System.out.println(noofMobilePiece);
+	int ModelNumber = 12345;
+	long MobileIMEINumber =12345678912l;
+	System.out.println(MobileIMEINumber);
+	float mobilePrice = 58345.80f;
+	System.out.println(mobilePrice);
+	boolean IOSDamaged = false;
+	System.out.println(IOSDamaged);
+}
+
+
+}
+
+
